4693a492698e6c71c8313782f6ed736fb250267e
maitrungnam007 <maitrungnam98@gmail.com>
2026-03-21 15:21:31 +0700

    Thong bao

--- backend/app/routes/thongbao.php
+++ backend/app/routes/thongbao.php
@@ -9,3 +9,4 @@ require_once __DIR__ . '/../controllers/ThongBaoController.php';
 Router::get('/thongbao', ['ThongBaoController', 'getMyNotifications']);
 Router::post('/thongbao/create', ['ThongBaoController', 'send']);
 Router::put('/thongbao/read/{id}', ['ThongBaoController', 'markRead']);
+Router::put('/thongbao/read-all', ['ThongBaoController', 'markAllRead']);
