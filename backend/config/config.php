d2cd4e158b4f4f9ba500bac57f5a9122ebbb890f
Tran Ha Thanh Thuy <thanhthuyy@gmail.com>
2026-03-24 01:01:35 +0700

    loi ket noi

---
+++ backend/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
+define('DB_NAME', 'tutoring_db');
+define('DB_USER', 'root');
+// define('DB_PASS', 'your_password');
+define('DB_PASS', 'root');
+
+define('JWT_SECRET', 'your_secret_key');
+define('JWT_EXPIRE', 86400 * 7); // 7 days
+
+define('MAIL_HOST', 'smtp.gmail.com');
+define('MAIL_PORT', 587);
+define('MAIL_USER', 'your_email@gmail.com');
+define('MAIL_PASS', 'your_password');
